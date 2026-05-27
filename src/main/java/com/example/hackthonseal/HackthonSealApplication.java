440b2ef4741d9496cb45c4674fc50437354b3ffe
baothanh4 <taobaothanh365@gmail.com>
2026-05-27 18:23:29 +0700

    First commit

---
+++ src/main/java/com/example/hackthonseal/HackthonSealApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackthonseal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackthonSealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackthonSealApplication.class, args);
+    }
+
+}
